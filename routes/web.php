995bc103d066d6c6694cfcd9f75cef77dc3f3aa3
soloden <akyztick20@gmail.com>
2020-07-02 15:17:46 +0300

    созданы роуты

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BookmarkController@index');
+Route::get('/add', 'BookmarkController@create');
+Route::post('/add', 'BookmarkController@store');
+Route::get('/{id}', 'BookmarkController@show')->where('id', '[0-9]+');
+Route::get('/delete/{id}', 'BookmarkController@destroy')->where('id', '[0-9]+');
