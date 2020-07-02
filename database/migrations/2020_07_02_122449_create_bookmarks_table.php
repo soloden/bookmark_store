0f3c7b0cfcb42a2f9abba8b968218a3943001f69
soloden <akyztick20@gmail.com>
2020-07-02 15:25:51 +0300

    создана миграция bookmarks

--- database/migrations/2020_07_02_122449_create_bookmarks_table.php
+++ database/migrations/2020_07_02_122449_create_bookmarks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBookmarksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bookmarks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('url')->nullable(false);
+            $table->string('title')->nullable(false);
+            $table->string('favicon')->nullable();
+            $table->string('keyword')->nullable();
+            $table->string('description')->nullable();
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bookmarks');
     }
 }
